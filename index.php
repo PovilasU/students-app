51b0117947cbe1233db5afbf3746fa727a62cb6f
PovilasU <povilas.urbonas85@gmail.com>
2025-11-28 06:26:17 +0200

    Add basic Hello world index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// index.php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Šita bus mūsų paraiškų sistemos pradžia.</p>
+</body>
+</html>
